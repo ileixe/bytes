0433f67085588801d2173f0c12ddeac8ff7d411d
Youseok Yang <yan@furiosa.ai>
2023-02-01 20:57:10 +0900

    Ignore Drop for BytesMut

--- src/bytes_mut.rs
+++ src/bytes_mut.rs
@@ -1051,12 +1051,13 @@ impl Drop for BytesMut {
         let kind = self.kind();
 
         if kind == KIND_VEC {
-            unsafe {
-                let (off, _) = self.get_vec_pos();
+            // XXX: ignore we are using BytesMut for unowned buffer
+            // unsafe {
+            //     let (off, _) = self.get_vec_pos();
 
-                // Vector storage, free the vector
-                let _ = rebuild_vec(self.ptr.as_ptr(), self.len, self.cap, off);
-            }
+            //     // Vector storage, free the vector
+            //     let _ = rebuild_vec(self.ptr.as_ptr(), self.len, self.cap, off);
+            // }
         } else if kind == KIND_ARC {
             unsafe { release_shared(self.data) };
         }
